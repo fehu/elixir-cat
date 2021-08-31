79fe1f88d31b3633ca001ed5bb10a81a9e1e96c6
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-08-31 16:38:44 -0500

    Refactor `Fiber`.

--- lib/cat/protocols/effect/fiber.ex
+++ lib/cat/protocols/effect/fiber.ex
@@ -1,11 +1,12 @@
 defprotocol Cat.Effect.Fiber do
   @moduledoc false
 
-  @type t(_x) :: term
+  @type t(_x) :: any
+  @type f(_x) :: any
 
-  @spec cancel(t(any)) :: t(no_return)
+  @spec cancel(t(any)) :: f(no_return)
   def cancel(ta)
 
-  @spec join(t(a)) :: t(MonadCancel.outcome(a)) when a: var
+  @spec join(t(a)) :: f(MonadCancel.outcome(a)) when a: var
   def join(ta)
 end
